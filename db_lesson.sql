83c7f2fddbbebcf2b116c5d52e16abb9a697f932
佐藤　知昭 <SatoTomoaki@MacBookProst.local>
2025-07-07 12:10:04 +0900

    db_lesson.sql内の記述を修正

--- db_lesson.sql
+++ db_lesson.sql
@@ -1,29 +1,29 @@
 -- Q1
 CREATE TABLE departments(
-    -> department_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    -> name VARCHAR(20) NOT NULL,
-    -> created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    -> updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    -> );
+  department_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(20) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  );
 -- Q2
 ALTER TABLE people ADD COLUMN department_id INT UNSIGNED AFTER email;
 -- Q3
 -- 追加する部署一覧(departments)
 INSERT INTO departments (name)
-    -> VALUES
-    -> ('営業'),
-    -> ('開発'),
-    -> ('経理'),
-    -> ('人事'),
-    -> ('情報システム');
+  VALUES
+  ('営業'),
+  ('開発'),
+  ('経理'),
+  ('人事'),
+  ('情報システム');
 -- 追加する人の条件(people)
 INSERT INTO people (person_id, name)
-    -> VALUES
-    -> ('4', '山本けんたろう'),
-    -> ('7', '矢野ちひろ'),
-    -> ('8', '佐藤あきら'),
-    -> ('9', '加藤まほ'),
-    -> ('10', '遠藤ようへい');
+  VALUES
+  ('4', '山本けんたろう'),
+  ('7', '矢野ちひろ'),
+  ('8', '佐藤あきら'),
+  ('9', '加藤まほ'),
+  ('10', '遠藤ようへい');
 UPDATE people
 SET age = CASE person_id
   WHEN person_id = 4 THEN '18'
@@ -48,27 +48,22 @@ UPDATE people SET gender = '2' WHERE person_id = 9;
 UPDATE people SET gender = '1' WHERE person_id = 10;
 -- 追加する日報の条件(reports)
 INSERT INTO reports (person_id, content) VALUES
-  -> (4, 'Report from 山本けんたろう - 1'),
-  -> (4, 'Report from 山本けんたろう - 2'),
-  -> (7, 'Report from 矢野ちひろ - 1'),
-  -> (7, 'Report from 矢野ちひろ - 2'),
-  -> (7, 'Report from 矢野ちひろ - 3'),
-  -> (7, 'Report from 矢野ちひろ - 4'),
-  -> (8, 'Report from 佐藤あきら - 1'),
-  -> (9, 'Report from 加藤まほ - 1'),
-  -> (10, 'Report from 遠藤ようへい - 1'),
-  -> (10, 'Report from 遠藤ようへい - 2');
+  (4, 'Report from 山本けんたろう - 1'),
+  (4, 'Report from 山本けんたろう - 2'),
+  (7, 'Report from 矢野ちひろ - 1'),
+  (7, 'Report from 矢野ちひろ - 2'),
+  (7, 'Report from 矢野ちひろ - 3'),
+  (7, 'Report from 矢野ちひろ - 4'),
+  (8, 'Report from 佐藤あきら - 1'),
+  (9, 'Report from 加藤まほ - 1'),
+  (10, 'Report from 遠藤ようへい - 1'),
+  (10, 'Report from 遠藤ようへい - 2');
 -- Q4
-UPDATE people SET department_id = '1' WHERE person_id = 1;
-UPDATE people SET department_id = '1' WHERE person_id = 2;
-UPDATE people SET department_id = '1' WHERE person_id = 3;
-UPDATE people SET department_id = '2' WHERE person_id = 4;
-UPDATE people SET department_id = '2' WHERE person_id = 5;
-UPDATE people SET department_id = '2' WHERE person_id = 6;
-UPDATE people SET department_id = '2' WHERE person_id = 7;
-UPDATE people SET department_id = '3' WHERE person_id = 8;
-UPDATE people SET department_id = '4' WHERE person_id = 9;
-UPDATE people SET department_id = '5' WHERE person_id = 10;
+UPDATE people SET department_id = '1' WHERE person_id IN (1, 2, 3);
+UPDATE people SET department_id = '2' WHERE person_id IN (4, 5, 6, 7);
+UPDATE people SET department_id = '3' WHERE person_id IN (8);
+UPDATE people SET department_id = '4' WHERE person_id IN (9);
+UPDATE people SET department_id = '5' WHERE person_id IN (10);
 -- Q5
 SELECT name, gender, age
 FROM people
